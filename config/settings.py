81dfd6fbadf663276aa081a1da3926ef7d4945b8
Juan David Salamanca <judasaca@dhcp-47-196.physik.hu-berlin.de>
2025-07-22 11:23:32 +0200

    Add settings

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,13 @@
-from pydantic import Field
+from pydantic import Field, Secret, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class _Settings(BaseSettings):
-    api_base_path: str = Field("localhost:3000")
+    api_base_path: str = Field("http://localhost:8000/fairdi/nomad/latest/api/v1")
+    username: str = Field()
+    password: SecretStr
 
-    model_config = SettingsConfigDict()
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
 settings = _Settings()
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from config import settings
+import requests
+
+api_base_path = settings.api_base_path
 
 
 def main():
-    print(settings)
+    response = requests.post(
+        api_base_path + "/auth/token",
+        data={"username": settings.username, "password": settings.password.get_secret_value()},
+    )
+    print(response.status_code, response.json())
 
 
 if __name__ == "__main__":
